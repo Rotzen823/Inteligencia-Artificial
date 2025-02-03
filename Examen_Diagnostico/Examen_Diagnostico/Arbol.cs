6ad89b421cf88293ae898db655cfde5e1ba2d88a
Rotzen823 <126202385+Rotzen823@users.noreply.github.com>
2025-02-03 11:47:10 -0700

    Examen Diagnostico

---
+++ Examen_Diagnostico/Examen_Diagnostico/Arbol.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Diagnostico
+{
+    public class Arbol
+    {
+        private Nodo root;
+
+        public Arbol()
+        {
+            this.root = null;
+        }
+
+        public bool vacio()
+        {
+            return root == null;
+        }
+
+        public void agregarNodo(string nombreNodo)
+        {
+            if (vacio())
+            {
+                root = new Nodo(nombreNodo);
+            }
+            else
+            {
+                agregarNodo(root, new Nodo(nombreNodo));
+            }
+        }
+
+        public void agregarNodo(Nodo raiz, Nodo nodo)
+        {
+            if (raiz.getNombre().CompareTo(nodo.getNombre()) == -1)
+            {
+                if (raiz.getNodoIzq() == null)
+                {
+                    raiz.setNodoIzq(nodo);
+                }
+                else
+                {
+                    agregarNodo(raiz.getNodoIzq(), nodo);
+                }
+            }
+            else
+            {
+                if (raiz.getNodoDer() == null)
+                {
+                    raiz.setNodoDer(nodo);
+                }
+                else
+                {
+                    agregarNodo(raiz.getNodoDer(), nodo);
+                }
+            }
+        }
+
+        public bool buscarNodo(string nombre)
+        {
+            if (vacio())
+            {
+                return false;
+            }
+
+
+        }
+
+        public bool buscarNodo(Nodo raiz, string nombre)
+        {
+            if(raiz == null)
+            {
+                return false;
+            }
+
+            if(raiz.getNombre() == nombre)
+            {
+                return true;
+            }
+
+            if(raiz.getNombre().CompareTo(nombre) == -1)
+            {
+                return buscarNodo(raiz.getNodoIzq(), nombre);
+            }
+
+            return buscarNodo(raiz.getNodoDer(), nombre);
+        }
+
+    }
+}
---
+++ Examen_Diagnostico/Examen_Diagnostico/Nodo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Diagnostico
+{
+    public class Nodo
+    {
+        private string nombre;
+        private Nodo nodoIzq;
+        private Nodo nodoDer;
+
+        public Nodo(string nombre)
+        {
+            this.nombre = nombre;
+            nodoIzq = null;
+            nodoDer = null;
+        }
+
+        public string getNombre()
+        {
+            return nombre;
+        }
+
+        public Nodo getNodoIzq()
+        {
+            return nodoIzq;
+        }
+
+        public void setNodoIzq(Nodo nodo)
+        {
+            this.nodoIzq = nodo;
+        }
+
+        public Nodo getNodoDer()
+        {
+            return nodoDer;
+        }
+
+        public void setNodoDer(Nodo nodo)
+        {
+            this.nodoDer = nodo;
+        }
+    }
+}
